5793691c801fd13b741d9bb8e55a8a3e2382d7e7
Garrett Everding <garrett.everding92@gmail.com>
2012-09-13 11:21:20 -0400

    Basic Script for loading locations of eggs in db

---
+++ loadEggs.js
@@ -0,0 +1,22 @@
+/* Script for loading up the Egg locations */
+
+/* mongo localhost/db loadEggs.js */
+var locations = [
+	[43.47865, 31.137751],
+	[23.71985, 61.502224],
+	[-105.918694, 35.50936],
+	[86.9233 ,27.9856],
+	[46.64,-19.39]
+]
+
+db.EggLoc.ensureIndex({loc: '2d'})
+
+for (var i = 0; i < locations.length; i++){
+	db.EggLoc.insert(
+		{
+			loc: {
+				lon : locations[i][0],
+				lat: locations[i][1]
+			}
+		})
+}
